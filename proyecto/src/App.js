d37df378aedd2cf2a8d08084d3293162c127e343
AliVasped200 <152574439+AndresVasped@users.noreply.github.com>
2024-05-16 16:28:08 -0500

    iniciando

---
+++ proyecto/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PrincipalPage from "./principal";
+import './App.css'; // Importa el archivo CSS
+
+function App() {
+  return (
+    <div className="App">
+      <PrincipalPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ proyecto/src/principal.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function PrincipalPage()
+{
+    return (
+        <>
+    <h1>In bloom</h1>
+    <a href="#menu">Menu</a>
+    <a href="#registrate">Registrate</a>
+    <a href="#carrito">Carrito</a>
+  </>
+    );
+}
+export  default PrincipalPage;
